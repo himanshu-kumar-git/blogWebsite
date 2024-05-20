4706ddb6aceef78504e62366d7b4b7b50c148632
HIMANSHU <115986142+himanshu-kumar-git@users.noreply.github.com>
2024-05-20 14:13:50 +0530

    navabar signuo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,41 +26,52 @@ export default function Nav() {
 
     // All NavList 
     const navList = (
-        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-                style={{ color: mode === 'dark' ? 'white' : 'white' }}
-            >
-                <Link to={'/'} className="flex items-center">
-                    Home
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-                style={{ color: mode === 'dark' ? 'white' : 'white' }}
-            >
-                <Link to={'/allblogs'} className="flex items-center">
-                    Blogs
-                </Link>
-            </Typography>
-            <Typography
-                as="li"
-                variant="small"
-                color="blue-gray"
-                className="p-1 font-normal"
-                style={{ color: mode === 'dark' ? 'white' : 'white' }}
-            >
-                <Link to={'/adminlogin'} className="flex items-center">
-                    Admin Login
-                </Link>
-            </Typography>
-        </ul>
+      <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-normal"
+          style={{ color: mode === "dark" ? "white" : "white" }}
+        >
+          <Link to={"/"} className="flex items-center">
+            Home
+          </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-normal"
+          style={{ color: mode === "dark" ? "white" : "white" }}
+        >
+          <Link to={"/allblogs"} className="flex items-center">
+            Blogs
+          </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-normal"
+          style={{ color: mode === "dark" ? "white" : "white" }}
+        >
+          <Link to={"/signup"} className="flex items-center">
+            Sign Up
+          </Link>
+        </Typography>
+        <Typography
+          as="li"
+          variant="small"
+          color="blue-gray"
+          className="p-1 font-normal"
+          style={{ color: mode === "dark" ? "white" : "white" }}
+        >
+          <Link to={"/adminlogin"} className="flex items-center">
+            Admin Login
+          </Link>
+        </Typography>
+      </ul>
     );
 
     return (
